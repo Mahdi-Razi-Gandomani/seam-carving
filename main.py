f4331c7f8cf3df76ee86b1620dd75126e6cc37c1
Mahdi Razi Gandomani <mahdi.razi099@gmail.com>
2025-12-03 23:22:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from cli import run_cli
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("Run with arguments, e.g.: python3 main.py input.jpg output.jpg --vertical 20 --gif")
+        print("Run with arguments -> python3 main.py input.jpg output.jpg --vertical 20")
+        sys.exit(1)
     else:
         run_cli()
